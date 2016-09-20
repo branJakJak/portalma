f699f9e8fb327226ac52c314933baf5159f1fc10
Your Name <you@example.com>
2016-09-21 00:13:11 +0800

    sa

---
+++ README.md
@@ -0,0 +1,14 @@
+Portal for pb.site8.co
+============================
+
+
+
+TODO
+
+    user management
+
+    allow admin to view submitted  by each agents
+
+
+
+
---
+++ config/db.php
@@ -0,0 +1,9 @@
+<?php
+
+return [
+    'class' => 'yii\db\Connection',
+    'dsn' => 'mysql:host=localhost;dbname=portal_site8',
+    'username' => 'root',
+    'password' => '',
+    'charset' => 'utf8',
+];

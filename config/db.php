2b9eab6c7ba65eca07c3124f8b71223502291f33
Your Name <you@example.com>
2016-09-22 03:13:46 +0800

    config

--- config/db.php
+++ config/db.php
@@ -2,8 +2,8 @@
 
 return [
     'class' => 'yii\db\Connection',
-    'dsn' => 'mysql:host=localhost;dbname=portal_site8',
-    'username' => 'root',
-    'password' => '',
+    'dsn' => 'mysql:host=localhost;dbname=site8_ma',
+    'username' => 'site8_ma',
+    'password' => 'XC2UaWu7OHL8Td5bZ1Kt',
     'charset' => 'utf8',
 ];
